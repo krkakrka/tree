81d8030eeab67255eb014bba51d975edf53bf06e
Karolis Lesevicius <karolis.lesevicius@gmail.com>
2020-03-31 12:42:44 +0300

    Recursive only

--- src/App.js
+++ src/App.js
@@ -2,23 +2,84 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function TreeRecursive({ node, onAddNode }) {
+  return (
+    <ul>
+      <li>{node.content}</li>
+      {node.children.length > 0
+        ? node.children.map(child => <TreeRecursive node={child} />)
+        : null
+      }
+      <li><button onClick={() => onAddNode(node)}>Add node</button></li>
+    </ul>
+  );
+}
+
+function TreeIterative({ node, onAddNode }) {
+  let level = 0;
+  const nodesStack = [];
+  const nodesWithLevels = [];
+
+  // nodesStack.push({ node, level });
+  // while (nodesStack.length) {
+  let currentNode = node;
+  while (true) {
+    while (currentNode) {
+
+    }
+    nodesStack.push(node.children[node.children.length - 1], node);
+    // handle single child?
+    node = node.children[0];
+
+    node = nodesStack.pop();
+
+    const { node, level } = nodesStack.pop();
+    nodesWithLevels.push({ node, level });
+    // nodesWithLevels.push(<ul><li>{node.content}</li></ul>);
+    if (node.children.length > 0) {
+      const childrenWithLevels = node.children.map(child => ({ node: child, level: level + 1 }))
+      nodesStack.push(...childrenWithLevels);
+    }
+  }
+
+  // const nodeToList = (node) => (
+  //   <ul>
+  //     <li>{node.content}</li>
+  //     {node.children.length > 0 ? node.children : null}
+  //   </ul>
+  // );
+
+  return (
+    <ul>
+      {nodesWithLevels.map(({ node, level }) => (
+        <li style={{ marginLeft: `${level * 10}px` }}>{node.content}</li>
+      ))}
+    </ul>
+  );
+}
+
+const testTree = {
+  content: 'root',
+  children: [
+    { content: 'child1', children: [
+      { content: 'child2', children: [
+        { content: 'child3', children: []}
+      ]}
+    ]},
+    { content: 'child1', children: []}
+  ]
+};
+
 function App() {
+  const [tree, setTree] = React.useState(testTree);
+  const handleAddNode = (parentNode) => {
+    const newNode = { content: Math.random(), children: [] };
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <TreeIterative node={tree} onAddNode={handleAddNode} /> */}
+      <TreeRecursive node={tree} onAddNode={handleAddNode} />
     </div>
   );
 }
